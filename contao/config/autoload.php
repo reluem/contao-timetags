98ae8307d573164351e4387b8128341379507ef2
reluem <lucas.rech@me.com>
2022-05-27 13:16:32 +0200

    added files

---
+++ contao/config/autoload.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2017 Leo Feyer
+ *
+ *
+ * PHP version 5
+ * @copyright  Martin Kozianka 2011-2017
+ * @author     Martin Kozianka <http://kozianka.de/>
+ * @package    timetags
+ * @license   LGPL 
+ * @filesource
+ */
+
+ClassLoader::addClasses(array(
+	'Timetags'    => 'system/modules/timetags/classes/Timetags.php'
+));
---
+++ contao/languages/en/default.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2017 Leo Feyer
+ *
+ *
+ * PHP version 5
+ * @copyright  Martin Kozianka 2011-2017
+ * @author     Martin Kozianka <http://kozianka.de/>
+ * @package    timetags
+ * @license    LGPL 
+ * @filesource
+ */
+
+
+$GLOBALS['TL_LANG']['FMD']['timetags_timesince_format']     = "[%s ago]:";
+$GLOBALS['TL_LANG']['FMD']['timetags_timesince']            = ["second", "minute", "hour", "day", "week", "month", "year", "decade"];
+$GLOBALS['TL_LANG']['FMD']['timetags_timesince_plural']     = ["seconds", "minutes", "hours", "days", "weeks", "months", "years", "decades"];
+
+$GLOBALS['TL_LANG']['FMD']['timetags_countdown_plural']     = ["seconds", "minutes", "hours", "days"];
+$GLOBALS['TL_LANG']['FMD']['timetags_countdown']            = ["second", "minute", "hour", "day"];
+
+
